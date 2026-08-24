da9321e4eee5e4ba6189b170ae7cbf5605644f1a
Rupesh155 <rudhg35@gmail.com>
2026-08-24 10:30:27 +0530

    connect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,61 +1,103 @@
+// // import React from 'react'
+// // import UseEffectDemo from './class_02/useEffectDemo'
+// // import NavBar from './class_03/NavBar'
+// // import { Route, Routes } from 'react-router-dom'
+// // import Home from './class_03/Home'
+// // import About from './class_03/About'
+// // import ProductList from './class_03/ProductList'
+// // import ProductsD from './class_03/ProductsD'
+// // // import StateExample from './class_01/StateExample'
+// // // https://github.com/Rupesh155
+
+// // const App = () => {
+// //   return (
+// //     <div>
+// //       {/* <StateExample/> */}
+// //        {/* <UseEffectDemo/> */}
+// //        <NavBar/>
+// //        {/* http://localhost:5173/p/1 */}
+// //        <Routes>
+// //         <Route   path='/'  element={<Home/>}/>
+// //         <Route   path='/about'  element={<About/>}/>
+// //         <Route   path='/list'  element={<ProductList/>}/>
+// //         <Route   path='/p/:id'  element={<ProductsD/>}/>
+
+
+
+// //        </Routes>
+
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+
+// // localhost:5173/
+
+
+
+
 // import React from 'react'
-// import UseEffectDemo from './class_02/useEffectDemo'
-// import NavBar from './class_03/NavBar'
-// import { Route, Routes } from 'react-router-dom'
-// import Home from './class_03/Home'
-// import About from './class_03/About'
-// import ProductList from './class_03/ProductList'
-// import ProductsD from './class_03/ProductsD'
-// // import StateExample from './class_01/StateExample'
-// // https://github.com/Rupesh155
+// // import Home from './class_03/Home'
+// import UseReduserDemo from './class_05/useReduserDemo'
+// import MemoDemo from './class_06/MemoDemo'
+// import Assignment from './class_07/Assignment'
 
 // const App = () => {
 //   return (
 //     <div>
-//       {/* <StateExample/> */}
-//        {/* <UseEffectDemo/> */}
-//        <NavBar/>
-//        {/* http://localhost:5173/p/1 */}
-//        <Routes>
-//         <Route   path='/'  element={<Home/>}/>
-//         <Route   path='/about'  element={<About/>}/>
-//         <Route   path='/list'  element={<ProductList/>}/>
-//         <Route   path='/p/:id'  element={<ProductsD/>}/>
-
-
-
-//        </Routes>
-
+//       {/* <Home/> */}
+//       {/* <UseReduserDemo/> */}
+//       {/* <MemoDemo/> */}
+//       <Assignment/>
 //     </div>
 //   )
 // }
 
 // export default App
 
-
-// localhost:5173/
-
-
-
-
-import React from 'react'
-// import Home from './class_03/Home'
-import UseReduserDemo from './class_05/useReduserDemo'
-import MemoDemo from './class_06/MemoDemo'
-import Assignment from './class_07/Assignment'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+ let [apiData,SetApiData]=    useState([])
+  useEffect(()=>{
+
+   async function api(){
+      let res=  await   axios.get("http://localhost:4000/")
+      console.log(res.data);
+      
+    }
+    api()
+
+
+
+          // fetch("http://localhost:4000/").then((res)=>{
+          //   return res.json()
+
+          // }).then((data)=>{
+          //   console.log(data);
+          //   SetApiData(data)
+          // })
+
+  },[])
+
   return (
     <div>
-      {/* <Home/> */}
-      {/* <UseReduserDemo/> */}
-      {/* <MemoDemo/> */}
-      <Assignment/>
+      {
+        apiData.map((val)=>{
+          return(<>
+          <h1>{val.id}</h1>
+          <h2>{val.name}</h2>
+          </>)
+
+        })
+      }
     </div>
   )
 }
 
 export default App
-
 
 
